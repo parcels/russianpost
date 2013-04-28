0313c3bbb4d9fb3edf2653c625f4f5fef493d00b
Artem Shitov <inbox@artemshitov.ru>
2013-04-28 04:20:13 +0400

    Tests for barcode validations

--- test/russianpost/barcode_test.rb
+++ test/russianpost/barcode_test.rb
@@ -6,4 +6,18 @@ class BarcodeTest < MiniTest::Unit::TestCase
     barcode = RussianPost::Barcode.new("RD025500807SE")
     assert_equal "bar RD025500807SE", "bar #{barcode}"
   end
+
+  def test_raises_on_invalid_barcode
+    ["123", "RR123456789EE"].each do |barcode|
+      assert_raises RussianPost::InvalidBarcode do
+        RussianPost::Barcode.new(barcode)
+      end
+    end
+  end
+
+  def test_doesnt_raise_on_valid_barcode
+    ["RD025500807SE", "12345678901234"].each do |barcode|
+        RussianPost::Barcode.new(barcode)
+    end
+  end
 end
